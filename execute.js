487d9fad12d8633f43f6fe772a3d83185a382ba5
alansyue <b123105@gmail.com>
2019-11-18 21:24:32 +0800

    upload

--- execute.js
+++ execute.js
@@ -1,17 +1,37 @@
-const rotateEvent = () => {
-  document.body.style.transform = 'rotate(180deg)';
+const countCredit = (grade) => {
+  var cookie = JSON.stringify
+  ({ 
+    Cookie: document.cookie,
+    Grade: grade
+  });
+
+  var ajax = new XMLHttpRequest()
+  var api = 'http://127.0.0.1:5000/api/count';
+  var resultCSS = "width:900px;height:auto;z-index:9999999;background-color:white;border:5px #FFAC55 solid;"
+  ajax.onreadystatechange = function(){
+    if(ajax.readyState == 4 && ajax.status == 200){
+      var msg = ajax.responseText;
+      var result = document.getElementById('login');
+      result.innerHTML = "<div style='"+resultCSS+"'>"+msg+"</div>";
+    }
+  }
+
+  //2.創建 post 請求
+  ajax.open('post', api);
+  // 設置 header
+  ajax.setRequestHeader("content-type","application/json; charset=UTF-8");
+  
+  //3.送出請求
+  ajax.send(cookie);
 };
-const reset = () => {
-  document.body.style.transform = '';
-}
 
 const onMessage = (message) => {
   switch (message.action) {
-    case 'ROTATE':
-      rotateEvent();
+    case 'before':
+      countCredit(1);
       break;
-    case 'RESET':
-      reset();
+    case 'after':
+      countCredit(2);
       break;
     default:
       break;
